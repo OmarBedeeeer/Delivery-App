176afd665fc9fa4de38a03261fd6e16f3ce295f8
bedeer <calabit_2@yahoo.com>
2024-04-26 12:31:00 +0300

    creating user auth and verification

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { AppError } from "./src/utils/error.handler.js";
+import userRouter from "./src/modules/router/user.router.js";
+import connectToDb from "./dbConnection/db.connection.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(userRouter);
 
 app.all("*", (req, res, next) => {
   throw new AppError("Can't find this route", 400);
@@ -15,5 +20,5 @@ app.use((error, req, res, next) => {
     stack,
   });
 });
-
+connectToDb();
 app.listen(process.env.PORT, () => console.log(`Server running on port!`));
